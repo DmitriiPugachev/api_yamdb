de2de46a44e348d2c5c4273bc21345729239d642
BigStanRus <Klimenko_sasha32@mail.ru>
2021-08-21 14:04:55 +0500

    не нужный коммит

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.contrib.auth import get_user_model
 from django.db.models.aggregates import Avg
+from rest_framework.exceptions import ValidationError
 from rest_framework import serializers, validators
 from rest_framework.relations import SlugRelatedField
 
